3632553b41a6aee64dad221721e3ea11419155c2
tqhuy2018 <87941180+tqhuy2018@users.noreply.github.com>
2022-02-27 15:15:20 +0700

    Update DHFPay.test.ts
    remove the redundant part commented

--- src/DHFPay.test.ts
+++ src/DHFPay.test.ts
@@ -1,39 +1,5 @@
 import DHFPay from "./DHFPay";
 
-// test('Create Payment', async ()=>{
-//     const client = new DHFPay({
-//         AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
-//     });
-//
-//     const createParams: CreatePaymentDTO = {
-//         amount: 2500000,
-//         comment: "test payment"
-//     }
-//     const result = await client.createPayment(createParams)
-//     console.log(result);
-// });
-
-
-// test('Get Payment', async ()=>{
-//     const client = new DHFPay({
-//         AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
-//     });
-//
-//     const result = await client.getPayment(6)
-//     console.log(result);
-//
-// });
-//
-// test('Get Payments', async ()=>{
-//     const client = new DHFPay({
-//         AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
-//     });
-//
-//     const result = await client.getPayments();
-//     console.log(result);
-//
-// });
-
 test('Get Transactions', async ()=>{
     const client = new DHFPay({
         AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
@@ -43,6 +9,3 @@ test('Get Transactions', async ()=>{
     console.log(result);
 
 });
-
-
-// 6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY
