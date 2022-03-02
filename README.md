505c81ed6cf21c6fa9cbad9f77ca28bab773a4e9
Enflow.io <konstantin@enflow.io>
2022-03-02 12:41:36 +0300

    Documentation and tests

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # DH Pay JS
-# DH Pay JS
+JS/TS SDK to integrate with DHFinance in minutes.
+# Install
+```sh
+ npm i dhf-pay-js -S
+```
+# Import
+```sh
+ import DHFPay from "dhf-pay-js";
+```
+
+# Create client 
+```sh
+ const client = new DHFPay({
+          AUTH_TOKEN: '##TOKEN##'
+      });
+```
+
+
+# Create payment
+ ```sh
+    const createParams: CreatePaymentDTO = {
+          amount: 2500000, 
+          comment: "test payment"
+    }
+    const result = await client.createPayment(createParams)
+ 
+```
+ 
+ # Payment status
+ ```sh
+    const paymentId = 1;
+    const result: PaymentInterface = await client.getPayment(paymentId)
+```
+ 
+ 
+ 
+ # Payments list
+ ```sh
+    const result: PaymentInterface[] = await client.getPayments();
+```
+  
+ 
+ # Transactions list
+ ```sh
+    const result: TransactionInterface[] = await client.getTransactions();
+```
+ 
+ 
+ # Interfaces
+  ```sh
+     interface PaymentInterface {
+         id: number
+         datetime: string
+         amount: string
+         status: 'Not_paid' | 'paid' | string
+         comment: string
+         text: string
+         store: StoreInterface
+     }
+
+    interface StoreInterface {
+        id: number
+        url: string
+        name: string
+        wallet: string
+        description: string
+        blocked: boolean
+    }
+
+    interface TransactionInterface {
+        id: number
+        status: string
+        email: string
+        updated: string
+        txHash: string
+        sender: string
+        amount: string
+        payment: PaymentInterface
+    }
+
+    interface CreatePaymentDTO {
+        amount: number
+        comment: string
+    }
+
+ ```
+ 
+ 
+# Run tests 
+```sh
+ npm test
+```
+ 
+ 
--- src/DHFPay.test.ts
+++ src/DHFPay.test.ts
@@ -1,48 +1,102 @@
 import DHFPay from "./DHFPay";
 
-// test('Create Payment', async ()=>{
-//     const client = new DHFPay({
-//         AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
-//     });
-//
-//     const createParams: CreatePaymentDTO = {
-//         amount: 2500000,
-//         comment: "test payment"
-//     }
-//     const result = await client.createPayment(createParams)
-//     console.log(result);
-// });
+const createPayment = jest.fn().mockImplementation(function () {
+    return {id: 12}
+});
+
+const getPayment = jest.fn().mockImplementation(function () {
+    return {
+        id: 6,
+        datetime: '2022-02-24T10:36:34.580Z',
+        amount: '2500000',
+        status: 'Not_paid',
+        comment: 'test payment',
+        type: null,
+        text: null,
+        store: {
+            id: 5,
+            url: 'https://enflow.io/callback',
+            name: 'Store1',
+            wallet: '01133ff8c7bab58c2067306c4ae3c8c08f10f0a9d60ab1b7489a5c88b78a4aba01',
+            description: '',
+            apiKey: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY',
+            blocked: false
+        }
+    }
+});
+const getPayments = jest.fn().mockImplementation(function () {
+    return [
+        {
+            id: 5,
+            datetime: '2022-02-24T10:32:23.758Z',
+            amount: '2500000',
+            status: 'Not_paid',
+            comment: 'test payment',
+            type: null,
+            text: null,
+            store: {
+                id: 5,
+                url: 'https://enflow.io/callback',
+                name: 'Store1',
+                wallet: '01133ff8c7bab58c2067306c4ae3c8c08f10f0a9d60ab1b7489a5c88b78a4aba01',
+                description: '',
+                apiKey: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY',
+                blocked: false
+            }
+        }]
+});
 
 
-// test('Get Payment', async ()=>{
-//     const client = new DHFPay({
-//         AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
-//     });
-//
-//     const result = await client.getPayment(6)
-//     console.log(result);
-//
-// });
-//
-// test('Get Payments', async ()=>{
-//     const client = new DHFPay({
-//         AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
-//     });
-//
-//     const result = await client.getPayments();
-//     console.log(result);
-//
-// });
+const getTransactions = jest.fn().mockImplementation(function () {
+    return []
+});
+
+
+const client = new DHFPay({
+    AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
+});
+
+client.createPayment = createPayment.bind(client)
+client.getPayment = getPayment.bind(client)
+client.getPayments = getPayments.bind(client)
+client.getTransactions = getTransactions.bind(client)
+
+
+test('Create Payment', async ()=>{
+
+    const createParams: CreatePaymentDTO = {
+        amount: 2500000,
+        comment: "test payment"
+    }
+    const result = await client.createPayment(createParams)
+    expect(result).toBeDefined()
+    expect(result).toHaveProperty('id')
+    expect(result?.id).toEqual(12)
+});
+
+
+test('Get Payment', async ()=>{
+    const result = await client.getPayment(6)
+    expect(result).toBeDefined()
+    expect(result).toHaveProperty('id')
+    expect(result).toHaveProperty('store')
+    expect(result).toHaveProperty('amount')
+    expect(result?.id).toEqual(6)
+
+});
+
+test('Get Payments', async () => {
+    const result = await client.getPayments();
+    expect(result).toBeDefined()
+    expect(Array.isArray(result)).toBe(true)
+
+});
 
 test('Get Transactions', async ()=>{
-    const client = new DHFPay({
-        AUTH_TOKEN: '6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY'
-    });
-
     const result = await client.getTransactions();
-    console.log(result);
+    expect(result).toBeDefined()
+    expect(Array.isArray(result)).toBe(true)
 
 });
 
 
-// 6bzKGw4Rcd508FbTZA02OW1mQglxSwJ1CXdY
